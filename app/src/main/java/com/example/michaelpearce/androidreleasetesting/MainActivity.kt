2bdfafbbd4451dab962b0c2c27440632e917b3db
Michael Pearce <michael.pearce@airtasker.com>
2018-09-14 11:28:42 +1000

    Add first feature

--- app/src/main/java/com/example/michaelpearce/androidreleasetesting/MainActivity.kt
+++ app/src/main/java/com/example/michaelpearce/androidreleasetesting/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.michaelpearce.androidreleasetesting
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textViewHelloWorld.text = getString(R.string.first_feature)
     }
 }
