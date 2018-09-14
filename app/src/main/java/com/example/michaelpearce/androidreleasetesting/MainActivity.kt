5ff412c8ca1b77df3c43b9061e223900f6d6c36b
Michael Pearce <33816671+michaelpearce-at@users.noreply.github.com>
2018-09-14 11:36:54 +1000

    Fix first feature (#2)

--- app/src/main/java/com/example/michaelpearce/androidreleasetesting/MainActivity.kt
+++ app/src/main/java/com/example/michaelpearce/androidreleasetesting/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.michaelpearce.androidreleasetesting
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         textViewHelloWorld.text = getString(R.string.first_feature)
+        Log.d("MainActivity", "First feature...")
     }
 }
